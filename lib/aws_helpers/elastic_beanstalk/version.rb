fec2cb44188d9564f90a93590281a6d5132384a4
Virginia Lee <virginia.lee@gmail.com>
2014-08-06 16:57:01 +1000

    Update aws_helper to separate application version upload and version deployment.

--- lib/aws_helpers/elastic_beanstalk/version.rb
+++ lib/aws_helpers/elastic_beanstalk/version.rb
@@ -8,24 +8,27 @@ module AwsHelpers
   module ElasticBeanstalk
     class Version
 
-      def initialize(application, environment, version, version_contents, zip_folder=false)
+      def initialize(application, environment, version)
         raise_argument_error 'application' unless application
         raise_argument_error 'environment' unless environment
         raise_argument_error 'version' unless version
-        raise_argument_error 'version_contents' unless version_contents
 
         @application = application
         @environment = environment
         @version = version
-
-        klass = zip_folder ? AwsHelpers::ElasticBeanstalk::VersionZipFolder : VersionFile
-        @version_file = klass.new(@application, @version, version_contents)
         @application_version = ApplicationVersion.new(Aws::ElasticBeanstalk::Client.new)
       end
 
-      def deploy
+      def upload(version_contents, zip_folder=false)
+        raise_argument_error 'version_contents' unless version_contents
+
+        klass = zip_folder ? AwsHelpers::ElasticBeanstalk::VersionZipFolder : VersionFile
+        @version_file = klass.new(@application, @version, version_contents)
         @version_file.upload_to_s3
         @application_version.create(@application, @version_file)
+      end
+
+      def deploy
         @application_version.deploy(@application, @environment, @version)
       end
 
