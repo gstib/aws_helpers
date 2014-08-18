6f929111160a31c2e4b2ee93253133312cbf088f
Grant Tibbey <granttibbey@hotmail.com>
2014-08-19 08:02:27 +1000

    refactoring

--- lib/aws_helpers/elastic_beanstalk/version.rb
+++ lib/aws_helpers/elastic_beanstalk/version.rb
@@ -8,28 +8,27 @@ module AwsHelpers
   module ElasticBeanstalk
     class Version
 
-      def initialize(application, environment, version)
+      def initialize
+        @application_version = ApplicationVersion.new(Aws::ElasticBeanstalk::Client.new)
+      end
+
+      def upload(application, version, version_contents, zip_folder=false)
+        raise_argument_error 'application' unless application
+        raise_argument_error 'version' unless version
+        raise_argument_error 'version_contents' unless version_contents
+
+        klass = zip_folder ? AwsHelpers::ElasticBeanstalk::VersionZipFolder : VersionFile
+        version_file = klass.new(application, version, version_contents)
+        version_file.upload_to_s3
+        @application_version.create(application, version_file)
+      end
+
+      def deploy(application, environment, version)
         raise_argument_error 'application' unless application
         raise_argument_error 'environment' unless environment
         raise_argument_error 'version' unless version
 
-        @application = application
-        @environment = environment
-        @version = version
-        @application_version = ApplicationVersion.new(Aws::ElasticBeanstalk::Client.new)
-      end
-
-      def upload(version_contents, zip_folder=false)
-        raise_argument_error 'version_contents' unless version_contents
-
-        klass = zip_folder ? AwsHelpers::ElasticBeanstalk::VersionZipFolder : VersionFile
-        @version_file = klass.new(@application, @version, version_contents)
-        @version_file.upload_to_s3
-        @application_version.create(@application, @version_file)
-      end
-
-      def deploy
-        @application_version.deploy(@application, @environment, @version)
+        @application_version.deploy(application, environment, version)
       end
 
       private
--- lib/aws_helpers/version.rb
+++ lib/aws_helpers/version.rb
@@ -1,3 +1,3 @@
 module AwsHelpers
-  VERSION = '0.2.2'
+  VERSION = '0.3.0'
 end
